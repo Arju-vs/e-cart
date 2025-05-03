f44898de1c87504e74d2d4d5991fa4bf41eb00ab
arju-vs <arjunsaseendran2@gmail.com>
2025-05-03 20:11:30 +0530

    third

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,14 @@
 import React, { useEffect } from 'react'
 import Header from '../Components/Header'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { fetchProducts } from '../redux/slices.js/productSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProducts } from '../redux/slices/productSlice'
 
 const Home = () => {
   const dispatch = useDispatch()
+  const {allProducts,loading,errorMsg} = useSelector(state=>state.productReducer)
+  console.log(allProducts,loading,errorMsg);
+  
 
   useEffect(()=>{
     dispatch(fetchProducts())
@@ -15,15 +18,32 @@ const Home = () => {
     <>
     <Header insideHome={true} />
       <div style={{paddingTop:'100px'}} className='container px-4 mx-auto'>
-        <div className="grid grid-cols-4 gap-4">
-          <div className="rounded border p-2 shadow">
-            <img width={'100%'} height={'200px'} src="https://specials-images.forbesimg.com/imageserve/65df2e0562b5d061b718a4af/960x0.jpg" alt="" />
-            <div className="text-center">
-              <h3 className="text-xl font-bold">title</h3>
-              <Link to={'/:id/view'} className='bg-violet-600 rounded p-1 mt-3 text-white inline-block'>View More....</Link>
-            </div>
-          </div>
+        { loading ?
+        <div className="flex justify-center items-center my-5 text-lg">
+          <img width={'100px'} height={'100px'} className='me-3' src="https://img.freepik.com/premium-vector/vector-loading-icon-vector-editable-element-design_898898-180.jpg?semt=ais_hybrid&w=740" alt="" />
+          Loading...
         </div>
+        :
+          <>
+          <div className="grid grid-cols-4 gap-4">
+            { allProducts?.length>0 ?
+              allProducts?.map(product=>(
+                <div key={product?.id} className="rounded border p-2 shadow">
+              <img width={'100%'} height={'200px'} src={product?.thumbnail} alt="" />
+              <div className="text-center">
+                <h3 className="text-xl font-bold">{product?.title}</h3>
+                <Link to={`/${product?.id}/view`} className='bg-violet-600 rounded p-1 mt-3 text-white inline-block'>View More....</Link>
+              </div>
+            </div>
+              ))
+            :
+            <div className="flex justify-center items-center font-bold text-red-600 my-5 text-lg">
+              Product Not Found!!!
+              </div>
+            }
+          </div>
+        </>
+        }
       </div>
     </>
   )
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,26 +1,46 @@
 import React from 'react'
 import Header from '../Components/Header'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeItem } from '../redux/slices/wishlistSlice'
 
 const Wishlist = () => {
+  const dispatch = useDispatch()
+  const userWishlist = useSelector(state=>state.wishlistReducer)
+  console.log(userWishlist);
+  
+
+
   return (
     <>
       <Header />
       <div style={{paddingTop:'100px'}} className='px-5'>
+        {
+          userWishlist?.length>0?
         <>
         <h1 className="text-4xl font-bold text-red-600">My Wishlist</h1>
         <div className="grid grid-cols-4 gap-4">
-          <div className="rounded border p-2 shadow">
-            <img width={'100%'} height={'200px'} src="https://specials-images.forbesimg.com/imageserve/65df2e0562b5d061b718a4af/960x0.jpg" alt="" />
+          {
+          userWishlist?.map(product=>(
+            <div className="rounded border p-2 shadow">
+            <img width={'100%'} height={'200px'} src={product?.thumbnail} alt="" />
             <div className="text-center">
-              <h3 className="text-xl font-bold">title</h3>
+              <h3 className="text-xl font-bold">{product?.title}</h3>
               <div className="flex justify-evenly mt-3">
-                <button className="text-xl"><i className="fa-solid fa-heart-circle-xmark text-red-500"></i></button>
+                <button onClick={()=>dispatch(removeItem(product?.id))} className="text-xl"><i className="fa-solid fa-heart-circle-xmark text-red-500"></i></button>
                 <button className="text-xl"><i className="fa-solid fa-cart-plus text-green-700"></i></button>
                 </div>            
             </div>
           </div>
+          ))
+          }
         </div>
         </>
+        :
+        <div className="flex flex-col justify-center items-center">
+          <img src="https://i.pinimg.com/736x/2e/ac/fa/2eacfa305d7715bdcd86bb4956209038.jpg" alt="" />
+          <h1 className="text-3xl text-red-600">Your Wishlist is empty!!</h1>
+        </div>
+        }
       </div>
     </>
   )
